e07a31e04ea3f1db2282c65fac80e8a2365b0479
Dunford Mainor <danmainor@outlook.com>
2018-10-16 10:39:06 +0300

    Full Project Done and complete

---
+++ SOURCES/InfoSacco1.1/Login_Page.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class Login_Page
+    Private Sub loginButton_Click(sender As Object, e As RoutedEventArgs) Handles loginButton.Click
+        Dim main As New mainPage
+        Me.NavigationService.Navigate(main)
+    End Sub
+
+    Private Sub password(sender As Object, e As TextCompositionEventArgs)
+        Throw New NotImplementedException()
+    End Sub
+
+
+End Class
